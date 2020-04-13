a8a4d32ecdba323e93b9962480a9c1b70819b404
Alan Curtis <alancurtis5000@gmail.com>
2020-04-13 15:21:53 -0600

    added css normalize, webpack is production

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Button from './Button.js';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Button />
         <h1>React App!</h1>
       </div>
     )
